4b8b113a5c6130bcfe686b9c158607c4274d2163
kaka <huxignhai1988@gmail.com>
2015-03-25 00:28:24 +0800

    fixed fetch with

--- lib/capistrano/user_hook/configuration.rb
+++ lib/capistrano/user_hook/configuration.rb
@@ -2,8 +2,20 @@ require 'capistrano/configuration'
 
 module Capistrano
   class Configuration
+
+    alias_method :fetch_without_feature, :fetch
+
+    def fetch_with_feature(*args)
+      name = args.first
+      user = fetch_root_users(name)
+      value = fetch_without_feature(*args)
+      user.nil? ? value : {role: value, root_user: user}
+    end
+
+    alias_method :fetch, :fetch_with_feature
+
     def roles_for(names)
-      root_user = fetch(:root_user)
+      root_user = fetch_without_feature(:root_user)
       if root_user
         {servers: servers.roles_for(names), options: {root_user: root_user} }
       elsif names.is_a?(Array) && names[0].is_a?(Hash)
@@ -11,6 +23,20 @@ module Capistrano
       else
         servers.roles_for(names)
       end
+    end
+
+    private 
+
+    def fetch_root_users(name)
+      users = (fetch_without_feature(:root_users) || {})
+      for k, v in users
+        if v.is_a?(Array) && v.include?(name)
+          return k
+        elsif v == name
+          return k
+        end
+      end
+      return nil
     end
   end
 end
--- spec/deploy.rb
+++ spec/deploy.rb
@@ -4,4 +4,6 @@ set :root_user, 'ferrari'
 
 role :web, '192.168.9.16'
 
-set :unicorn_roles, {role: :web, root_use: 'prod'}
+set :unicorn_roles, :web
+
+set :root_users, {prod: [:unicorn_roles]}
